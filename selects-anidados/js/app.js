c205174a88a8c6f5b9e43e4e7763ab0dcc3a9233
Rodrigo Leite <rodrigoleite1073@gmail.com>
2022-09-17 03:04:40 -0500

    falta colocar bander del pais

--- selects-anidados/js/app.js
+++ selects-anidados/js/app.js
@@ -1,8 +1,13 @@
 const apiToken =
     "T7OqOkiabiqR7BRosIYObcVm-Ov0GYjI1Hg5QKCTDm-qADs6hJGoigla7FikaJfu01w",
   userEmail = "rodrigoleite1073@gmail.com",
-  api = "https://www.universal-tutorial.com/api/";
-
+  api = "https://www.universal-tutorial.com/api/",
+  d = document,
+  $country = d.getElementById("select-country"),
+  $state = d.getElementById("select-state"),
+  $city = d.getElementById("select-city"),
+  $errorMessage = d.querySelector(".error-message");
+let token;
 const getAuthToken = async () => {
   let authToken;
   try {
@@ -23,28 +28,96 @@ const getAuthToken = async () => {
       };
     }
     return authToken;
-  } catch (err) {}
+  } catch (err) {
+    console.error(err);
+    let message = `${err.statusText}` || "Error de autorizacion";
+    $errorMessage.textContent = `Error ${err.status}:${message}`;
+  }
 };
 
-const getContries = async (auth) => {
-  console.log(auth);
+const getContries = async () => {
+  token = await getAuthToken();
   try {
     const res = await fetch(`${api}countries/`, {
         headers: {
-          Authorization: "Bearer " + auth,
+          Authorization: "Bearer " + token,
           Accept: "application/json",
           "Access-Control-Allow-Origin": "*",
         },
       }),
       json = await res.json();
-    console.log(json);
     if (!res.ok) {
       throw {
         status: res.status,
         statusText: res.statusText,
       };
     }
-  } catch (err) {}
+    let $options = `<option value="">Select a country</option>`;
+    json.forEach((el) => {
+      $options += `<option value="${el.country_name}">${el.country_name}</option>`;
+    });
+    $country.innerHTML = $options;
+  } catch (err) {
+    console.error(err);
+    let message = `${err.statusText}` || "Error al cargar los paises";
+    $errorMessage.textContent = `Error ${err.status}:${message}`;
+  }
 };
 
-getAuthToken().then((auth) => getContries(auth));
+const getStates = async (country) => {
+  fetch(`${api}states/${country}`, {
+    headers: {
+      Authorization: "Bearer " + token,
+      Accept: "application/json",
+      "Access-Control-Allow-Origin": "*",
+    },
+  })
+    .then((res) => (res.ok ? res.json() : Promise.reject(res)))
+    .then((json) => {
+      let $options = `<option value="">Select a state</option>`;
+      json.forEach((el) => {
+        $options += `<option value="${el.state_name}">${el.state_name}</option>`;
+      });
+      $state.innerHTML = $options;
+      $state.removeAttribute("disabled", "");
+    })
+    .catch((err) => {
+      console.error(err);
+      let message = `${err.statusText}` || "Error al cargar los estados";
+      $errorMessage.textContent = `Error ${err.status}:${message}`;
+    });
+};
+
+const getCities = async (state) => {
+  console.log(state);
+  fetch(`${api}cities/${state}`, {
+    headers: {
+      Authorization: "Bearer " + token,
+      Accept: "application/json",
+      "Access-Control-Allow-Origin": "*",
+    },
+  })
+    .then((res) => res.json())
+    .then((json) => {
+      let $options = `<option value="">Select a city</option>`;
+      json.forEach((el) => {
+        $options += `<option value="${el.city_name}">${el.city_name}</option>`;
+      });
+      $city.innerHTML = $options;
+      $city.removeAttribute("disabled", "");
+    })
+    .catch((err) => {
+      console.error(err);
+      let message = `${err.statusText}` || "Error al cargar los estados";
+      $errorMessage.textContent = `Error ${err.status}:${message}`;
+    });
+};
+
+d.addEventListener("DOMContentLoaded", getContries);
+d.addEventListener("change", (e) => {
+  if (e.target === $country) {
+    getStates($country.value);
+  } else if (e.target === $state) {
+    getCities($state.value);
+  }
+});
